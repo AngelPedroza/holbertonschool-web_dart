1a96112b42a1e306e591fbec88e625bbf9df1593
angel.pedroza <55103543+AngelPedroza@users.noreply.github.com>
2024-02-26 16:54:27 -0500

    [Fix] change iterate map

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,23 +1,26 @@
 int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
-  int pointsA = 0;
-  int pointsB = 0;
+  num pointsA = 0;
+  num pointsB = 0;
 
-  for (var key in teamA.keys) {
+  teamA.forEach((key, value) {
     if (key == 'Free throws') {
-      pointsA += teamA['Free throws']!;
-      pointsB += teamB['Free throws']!;
+      pointsA += value;
+    } else if (key == '2 pointer') {
+      pointsA += value * 2;
+    } else if (key == '3 pointer') {
+      pointsA += value * 3;
     }
+  });
 
-    if (key == '2 pointer') {
-      pointsA += teamA['2 pointer']! * 2;
-      pointsB += teamB['2 pointer']! * 2;
+  teamB.forEach((key, value) {
+    if (key == 'Free throws') {
+      pointsB += value;
+    } else if (key == '2 pointer') {
+      pointsB += value * 2;
+    } else if (key == '3 pointer') {
+      pointsB += value * 3;
     }
-
-    if (key == '3 pointer') {
-      pointsA += teamA['3 pointer']! * 3;
-      pointsB += teamB['3 pointer']! * 3;
-    }
-  }
+  });
 
   if (pointsA > pointsB)
     return 1;
