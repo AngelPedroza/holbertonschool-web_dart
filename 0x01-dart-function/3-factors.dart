c9d1e691759fa0cae50419d62acae70f1125f812
angel.pedroza <55103543+AngelPedroza@users.noreply.github.com>
2024-02-26 15:30:51 -0500

    [Fix] Change conditional order

--- 0x01-dart-function/3-factors.dart
+++ 0x01-dart-function/3-factors.dart
@@ -1,10 +1,10 @@
 int fact(int f) {
-  if (f <= 0) {
+  if (f <= 0)
     return 0;
-  } else if (f == 1) {
-    return 1;
-  }
 
-  int num = fact(f - 1);
-  return f * num;
+  if (f == 1)
+    return 1;
+
+  else
+    return f * fact(f - 1);;
 }
